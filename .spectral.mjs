6e44ebe20ca352b52b3cbac0bb089bc19bdfddc1
devops@stoplight.io <devops@stoplight.io>
2026-06-12 15:57:42 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://onprem-qa-online.stoplight-dev.com/api/v1/projects/cHJqOjE3/spectral.js?branch_slug=main&token=7a491ab9-3e0a-4009-9b3e-1e99e4c634df";
+export default { extends: ruleset };
